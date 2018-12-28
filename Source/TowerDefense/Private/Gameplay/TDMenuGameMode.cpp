b0c3b945a32686f721afd0cf50d2d1ac9663c9fb
haiaimi <wanghai1997@outlook.com>
2018-12-28 20:11:57 +0800

    添加Menu状态

---
+++ Source/TowerDefense/Private/Gameplay/TDMenuGameMode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TDMenuGameMode.h"
+#include "UI/TDMenuHUD.h"
+
+
+
+
+ATDMenuGameMode::ATDMenuGameMode()
+{
+	HUDClass = ATDMenuHUD::StaticClass();
+}
---
+++ Source/TowerDefense/Public/UI/TDMenuHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TDMenuHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ATDMenuHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	ATDMenuHUD();
+	
+	
+};
